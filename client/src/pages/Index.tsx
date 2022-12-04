344c921d5375b9166a4458c69b56a8ebc8994e8b
Donald Iloekwe <donytex4u@gmail.com>
2022-12-04 14:23:47 +0000

    add some animation in landing page

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   chakra,
   Box,
@@ -12,8 +12,63 @@ import {
 } from '@chakra-ui/react';
 const Hero = require('../public/images/hero.png');
 import { Link, animateScroll as scroll } from 'react-scroll';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
+const container = {
+  show: {
+    transition: {
+      staggerChildren: 0.35,
+    },
+  },
+};
+
+const item = {
+  hidden: {
+    opacity: 0,
+    y: 200,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.3],
+      duration: 1.6,
+    },
+  },
+  exit: {
+    opacity: 0,
+    y: -200,
+    transition: {
+      ease: 'easeInOut',
+      duration: 0.8,
+    },
+  },
+};
 
 const Index = () => {
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+  });
+  const animation = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      animation.start({
+        x: 0,
+        opacity: 1,
+        y: 0,
+        transition: {
+          type: 'spring',
+          duration: 1.2,
+        },
+      });
+    }
+    if (!inView) {
+      animation.start({ x: '-100vw' });
+    }
+  }, [inView]);
+
   const Feature = (props: any) => {
     return (
       <Flex>
@@ -55,85 +110,62 @@ const Index = () => {
       pb={{ base: '5rem', md: '3rem' }}
       bg={useColorModeValue('gray.50', 'gray.800')}
     >
-      <Box
-        w={{ base: 'full', md: 11 / 12, xl: 9 / 12 }}
-        mx='auto'
-        textAlign={{ base: 'left', md: 'center' }}
+      <motion.div
+        variants={container}
+        initial='hidden'
+        animate='show'
+        exit='exit'
       >
-        <chakra.h1
-          mb={6}
-          fontSize={{ base: '4xl', md: '6xl' }}
-          fontWeight='bold'
-          lineHeight='none'
-          letterSpacing={{ base: 'normal', md: 'tight' }}
-          color='gray.700'
-          _dark={{ color: 'gray.100' }}
+        <Box
+          w={{ base: 'full', md: 11 / 12, xl: 9 / 12 }}
+          mx='auto'
+          textAlign={{ base: 'left', md: 'center' }}
         >
-          Track all your{' '}
-          <Text
-            display={{ base: 'block', lg: 'inline' }}
-            w='full'
-            bgClip='text'
-            bgGradient='linear(to-r, green.400,linkedin.500)'
-            fontWeight='extrabold'
+          <chakra.h1
+            mb={6}
+            fontSize={{ base: '4xl', md: '6xl' }}
+            fontWeight='bold'
+            lineHeight='none'
+            letterSpacing={{ base: 'normal', md: 'tight' }}
+            color='gray.700'
+            _dark={{ color: 'gray.100' }}
           >
-            job applications
-          </Text>{' '}
-          in a single place.
-        </chakra.h1>
-        <chakra.p
-          px={{ base: 0, lg: 15 }}
-          mb={6}
-          fontSize={{ base: 'lg', md: 'xl' }}
-          color='gray.600'
-          _dark={{ color: 'gray.300' }}
-        >
-          Todojobs is a web application that allows to you record and keep track
-          of your job applications. You can now ditch excel and use use todojobs
-          which is easier and faster to use. Categorize and seamlessly manage
-          your job applications.
-        </chakra.p>
-        <Stack
-          direction={{ base: 'column', sm: 'row' }}
-          mb={{ base: 4, md: 8 }}
-          spacing={2}
-          justifyContent={{ sm: 'left', md: 'center' }}
-        >
-          <Button
-            as='a'
-            variant='solid'
-            color='white'
-            bg='linkedin.500'
-            _hover={{ bg: 'linkedin.600' }}
-            display='inline-flex'
-            alignItems='center'
-            justifyContent='center'
-            w={{ base: 'full', sm: 'auto' }}
-            mb={{ base: 2, sm: 0 }}
-            size='lg'
-            cursor='pointer'
-            href='/login'
+            Track all your{' '}
+            <Text
+              display={{ base: 'block', lg: 'inline' }}
+              w='full'
+              bgClip='text'
+              bgGradient='linear(to-r, green.400,linkedin.500)'
+              fontWeight='extrabold'
+            >
+              job applications
+            </Text>{' '}
+            in a single place.
+          </chakra.h1>
+          <chakra.p
+            px={{ base: 0, lg: 15 }}
+            mb={6}
+            fontSize={{ base: 'lg', md: 'xl' }}
+            color='gray.600'
+            _dark={{ color: 'gray.300' }}
           >
-            Get Started
-            <Icon boxSize={4} ml={1} viewBox='0 0 20 20' fill='currentColor'>
-              <path
-                fillRule='evenodd'
-                d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
-                clipRule='evenodd'
-              />
-            </Icon>
-          </Button>
-          <Link
-            activeClass='active'
-            to='howItWorks'
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={500}
+            Todojobs is a web application that allows to you record and keep
+            track of your job applications. You can now ditch excel and use use
+            todojobs which is easier and faster to use. Categorize and
+            seamlessly manage your job applications.
+          </chakra.p>
+          <Stack
+            direction={{ base: 'column', sm: 'row' }}
+            mb={{ base: 4, md: 8 }}
+            spacing={2}
+            justifyContent={{ sm: 'left', md: 'center' }}
           >
             <Button
               as='a'
-              colorScheme='gray'
+              variant='solid'
+              color='white'
+              bg='linkedin.500'
+              _hover={{ bg: 'linkedin.600' }}
               display='inline-flex'
               alignItems='center'
               justifyContent='center'
@@ -141,57 +173,41 @@ const Index = () => {
               mb={{ base: 2, sm: 0 }}
               size='lg'
               cursor='pointer'
-              href='#howItWorks'
+              href='/login'
             >
-              How it Works
+              Get Started
               <Icon boxSize={4} ml={1} viewBox='0 0 20 20' fill='currentColor'>
                 <path
                   fillRule='evenodd'
-                  d='M6.672 1.911a1 1 0 10-1.932.518l.259.966a1 1 0 001.932-.518l-.26-.966zM2.429 4.74a1 1 0 10-.517 1.932l.966.259a1 1 0 00.517-1.932l-.966-.26zm8.814-.569a1 1 0 00-1.415-1.414l-.707.707a1 1 0 101.415 1.415l.707-.708zm-7.071 7.072l.707-.707A1 1 0 003.465 9.12l-.708.707a1 1 0 001.415 1.415zm3.2-5.171a1 1 0 00-1.3 1.3l4 10a1 1 0 001.823.075l1.38-2.759 3.018 3.02a1 1 0 001.414-1.415l-3.019-3.02 2.76-1.379a1 1 0 00-.076-1.822l-10-4z'
+                  d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
                   clipRule='evenodd'
                 />
               </Icon>
             </Button>
-          </Link>
-        </Stack>
-      </Box>
-      <Box
-        w={{ base: 'full', md: 10 / 12 }}
-        mx='auto'
-        mt={20}
-        textAlign='center'
-      >
-        <Image
-          w='full'
-          rounded='lg'
-          shadow='2xl'
-          src={Hero}
-          alt='todojobs application dashboard'
-        />
-      </Box>
-      <Flex
-        justifyContent='center'
-        alignItems='center'
-        mt='10rem'
-        id='howItWorks'
-        className='scrollTo'
-      >
-        <Box py={12} bg='white' _dark={{ bg: 'gray.700' }} rounded='xl'>
-          <Box maxW='7xl' mx='auto' px={{ base: 4, lg: 8 }} pb={4}>
-            <Box textAlign='center'>
-              <chakra.h2
-                _light={{ color: 'brand.600' }}
-                fontSize='1.1rem'
-                fontWeight='semibold'
-                textTransform='uppercase'
-                letterSpacing='wide'
-                pb={4}
+            <Link
+              activeClass='active'
+              to='howItWorks'
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <Button
+                as='a'
+                colorScheme='gray'
+                display='inline-flex'
+                alignItems='center'
+                justifyContent='center'
+                w={{ base: 'full', sm: 'auto' }}
+                mb={{ base: 2, sm: 0 }}
+                size='lg'
+                cursor='pointer'
+                href='#howItWorks'
               >
                 How it Works
                 <Icon
                   boxSize={4}
                   ml={1}
-                  mb={1}
                   viewBox='0 0 20 20'
                   fill='currentColor'
                 >
@@ -201,83 +217,145 @@ const Index = () => {
                     clipRule='evenodd'
                   />
                 </Icon>
-              </chakra.h2>
-              <chakra.p
-                mt={2}
-                fontSize={{ base: '3xl', sm: '4xl' }}
-                lineHeight='8'
-                fontWeight='extrabold'
-                fontFamily="'Overpass', sans-serif"
-                letterSpacing='tight'
-                _light={{ color: 'gray.700' }}
-              >
-                Track your job applications with ease
-              </chakra.p>
-              <chakra.p
-                mt={4}
-                maxW='2xl'
-                fontSize='xl'
-                mx={{ lg: 'auto' }}
-                color='gray.500'
-                _dark={{ color: 'gray.400' }}
-              >
-                You can finally get to keep track of all your job applications
-                as well as record the status of your applications. You can also
-                add to your google calendar any job application.
-              </chakra.p>
-            </Box>
-
-            <Box mt={10}>
-              <Stack
-                spacing={{ base: 10, md: 0 }}
-                display={{ md: 'grid' }}
-                gridTemplateColumns={{ md: 'repeat(2,1fr)' }}
-                gridColumnGap={{ md: 8 }}
-                gridRowGap={{ md: 10 }}
-              >
-                <Feature
-                  title='Categorize your applications'
-                  icon={<i className='fa-solid fa-list'></i>}
-                >
-                  You can categorize your applications by creating different
-                  categories and adding your job applications to any
-                  corresponding category. You can also edit or delete your
-                  categories.
-                </Feature>
-
-                <Feature
-                  title='Manage application status'
-                  icon={
-                    <i className='fa-solid fa-temperature-three-quarters'></i>
-                  }
-                >
-                  You can manage your application status. You can change to
-                  Rejected, Assessment, Interview or set it to closed. It is
-                  automatically set to close when the end date reaches.
-                </Feature>
-
-                <Feature
-                  title='Robust job filter'
-                  icon={<i className='fa-solid fa-arrow-up-wide-short'></i>}
-                >
-                  Filter between job applications using the category, status,
-                  and the job board or website. The search feature also adds and
-                  extra way to narrow down what you are looing for.
-                </Feature>
-
-                <Feature
-                  title='Manage job application'
-                  icon={<i className='fa-solid fa-copy'></i>}
-                >
-                  You can delete or edit your job applications with easy. You
-                  can add notes to your job applications. Job applications with
-                  notes display the note count.
-                </Feature>
-              </Stack>
-            </Box>
-          </Box>
+              </Button>
+            </Link>
+          </Stack>
         </Box>
-      </Flex>
+        <Box
+          w={{ base: 'full', md: 10 / 12 }}
+          mx='auto'
+          mt={20}
+          textAlign='center'
+        >
+          <motion.div variants={item}>
+            <Image
+              w='full'
+              rounded='lg'
+              shadow='2xl'
+              src={Hero}
+              alt='todojobs application dashboard'
+            />
+          </motion.div>
+        </Box>
+        <Box as='div' ref={ref}>
+          <Flex
+            justifyContent='center'
+            alignItems='center'
+            mt='10rem'
+            id='howItWorks'
+            className='scrollTo'
+          >
+            <motion.div animate={animation}>
+              <Box py={12} bg='white' _dark={{ bg: 'gray.700' }} rounded='xl'>
+                <Box maxW='7xl' mx='auto' px={{ base: 4, lg: 8 }} pb={4}>
+                  <Box textAlign='center'>
+                    <chakra.h2
+                      _light={{ color: 'brand.600' }}
+                      fontSize='1.1rem'
+                      fontWeight='semibold'
+                      textTransform='uppercase'
+                      letterSpacing='wide'
+                      pb={4}
+                    >
+                      How it Works
+                      <Icon
+                        boxSize={4}
+                        ml={1}
+                        mb={1}
+                        viewBox='0 0 20 20'
+                        fill='currentColor'
+                      >
+                        <path
+                          fillRule='evenodd'
+                          d='M6.672 1.911a1 1 0 10-1.932.518l.259.966a1 1 0 001.932-.518l-.26-.966zM2.429 4.74a1 1 0 10-.517 1.932l.966.259a1 1 0 00.517-1.932l-.966-.26zm8.814-.569a1 1 0 00-1.415-1.414l-.707.707a1 1 0 101.415 1.415l.707-.708zm-7.071 7.072l.707-.707A1 1 0 003.465 9.12l-.708.707a1 1 0 001.415 1.415zm3.2-5.171a1 1 0 00-1.3 1.3l4 10a1 1 0 001.823.075l1.38-2.759 3.018 3.02a1 1 0 001.414-1.415l-3.019-3.02 2.76-1.379a1 1 0 00-.076-1.822l-10-4z'
+                          clipRule='evenodd'
+                        />
+                      </Icon>
+                    </chakra.h2>
+                    <chakra.p
+                      mt={2}
+                      fontSize={{ base: '3xl', sm: '4xl' }}
+                      lineHeight='8'
+                      fontWeight='extrabold'
+                      fontFamily="'Overpass', sans-serif"
+                      letterSpacing='tight'
+                      _light={{ color: 'gray.700' }}
+                    >
+                      Track your job applications with ease
+                    </chakra.p>
+                    <chakra.p
+                      mt={4}
+                      maxW='2xl'
+                      fontSize='xl'
+                      mx={{ lg: 'auto' }}
+                      color='gray.500'
+                      _dark={{ color: 'gray.400' }}
+                    >
+                      You can finally get to keep track of all your job
+                      applications as well as record the status of your
+                      applications. You can also add to your google calendar any
+                      job application.
+                    </chakra.p>
+                  </Box>
+
+                  <Box mt={10}>
+                    <Stack
+                      spacing={{ base: 10, md: 0 }}
+                      display={{ md: 'grid' }}
+                      gridTemplateColumns={{ md: 'repeat(2,1fr)' }}
+                      gridColumnGap={{ md: 8 }}
+                      gridRowGap={{ md: 10 }}
+                    >
+                      <Feature
+                        title='Categorize your applications'
+                        icon={<i className='fa-solid fa-list'></i>}
+                      >
+                        You can categorize your applications by creating
+                        different categories and adding your job applications to
+                        any corresponding category. You can also edit or delete
+                        your categories.
+                      </Feature>
+
+                      <Feature
+                        title='Manage application status'
+                        icon={
+                          <i className='fa-solid fa-temperature-three-quarters'></i>
+                        }
+                      >
+                        You can manage your application status. You can change
+                        to Rejected, Assessment, Interview or set it to closed.
+                        It is automatically set to close when the end date
+                        reaches.
+                      </Feature>
+
+                      <Feature
+                        title='Robust job filter'
+                        icon={
+                          <i className='fa-solid fa-arrow-up-wide-short'></i>
+                        }
+                      >
+                        Filter between job applications using the category,
+                        status, and the job board or website. The search feature
+                        also adds and extra way to narrow down what you are
+                        looing for.
+                      </Feature>
+
+                      <Feature
+                        title='Manage job application'
+                        icon={<i className='fa-solid fa-copy'></i>}
+                      >
+                        You can delete or edit your job applications with easy.
+                        You can add notes to your job applications. Job
+                        applications with notes display the note count.
+                      </Feature>
+                    </Stack>
+                  </Box>
+                </Box>
+              </Box>
+            </motion.div>
+          </Flex>
+        </Box>
+      </motion.div>
     </Box>
   );
 };
